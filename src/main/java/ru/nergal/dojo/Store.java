5228fc861fdb536341579ea514f8814025818165
Евгений Терехов <nergal@psu.ru>
2024-03-13 08:24:53 +0400

    feat: created weekdaysSchedule

--- src/main/java/ru/nergal/dojo/Store.java
+++ src/main/java/ru/nergal/dojo/Store.java
@@ -1,5 +1,29 @@
 package ru.nergal.dojo;
 
-public class Store {
+import java.time.LocalDate;
 
+public class Store {
+    private final String weekdaysOpen;
+    private final String weekdaysClose;
+
+    public Store(String weekdaysSchedule) {
+        final String[] weekdaysHours = weekdaysSchedule.split(" - ");
+        this.weekdaysOpen = weekdaysHours[0];
+        this.weekdaysClose = weekdaysHours[1];
+    }
+
+    public String statusFor(int year, int month, int day, int hour, int minute) {
+        final String currentTime = String.format("%02d:%02d", hour, minute);
+        final int dayOfWeek = LocalDate.of(year, month, day).getDayOfWeek().getValue();
+
+        if (currentTime.compareTo(weekdaysOpen) < 0) {
+            return String.format("Opens at %s", weekdaysOpen);
+        } else if (currentTime.compareTo(weekdaysClose) < 0) {
+            return String.format("Closes at %s", weekdaysClose);
+        } else if (dayOfWeek > 4) {
+            return String.format("Opens on Monday at %s", weekdaysOpen);
+        } else {
+            return String.format("Opens tomorrow at %s", weekdaysOpen);
+        }
+    }
 }
--- src/test/java/ru/nergal/dojo/StoreTest.java
+++ src/test/java/ru/nergal/dojo/StoreTest.java
@@ -4,9 +4,40 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StoreTest {
+
+    public static final Store WEEKDAYS_ONLY_10_TO_20 = new Store("10:00 - 20:00");
+
+    public static final Store WEEKDAYS_ONLY_930_TO_1730 = new Store("09:30 - 17:30");
+
     @Test
-    void shouldInstantiate() {
-        Store actual =  new Store();
-        assertThat(actual).isNotNull();
+    void whenShopIsClosedOnAWorkdayMorning_thenItShouldOpenToday() {
+        assertThat(WEEKDAYS_ONLY_10_TO_20.statusFor(2024, 3, 11, 9, 0))
+            .isEqualTo("Opens at 10:00");
+        assertThat(WEEKDAYS_ONLY_930_TO_1730.statusFor(2024, 3, 11, 8, 0))
+            .isEqualTo("Opens at 09:30");
+    }
+
+    @Test
+    void whenShopIsClosedOnAWorkdayEvening_thenItShouldOpenTomorrow() {
+        assertThat(WEEKDAYS_ONLY_10_TO_20.statusFor(2024, 3, 11, 21, 0))
+            .isEqualTo("Opens tomorrow at 10:00");
+        assertThat(WEEKDAYS_ONLY_930_TO_1730.statusFor(2024, 3, 11, 18, 0))
+            .isEqualTo("Opens tomorrow at 09:30");
+    }
+
+    @Test
+    void whenShopIsOpen_thenItShouldCloseInTheEvening() {
+        assertThat(WEEKDAYS_ONLY_10_TO_20.statusFor(2024, 3, 11, 15, 0))
+            .isEqualTo("Closes at 20:00");
+        assertThat(WEEKDAYS_ONLY_930_TO_1730.statusFor(2024, 3, 11, 15, 0))
+            .isEqualTo("Closes at 17:30");
+    }
+
+    @Test
+    void whenShopIsClosedOnFridayEvening_thenItShouldOpenOnMonday() {
+        assertThat(WEEKDAYS_ONLY_10_TO_20.statusFor(2024, 3, 15, 21, 0))
+            .isEqualTo("Opens on Monday at 10:00");
+        assertThat(WEEKDAYS_ONLY_930_TO_1730.statusFor(2024, 3, 15, 18, 0))
+            .isEqualTo("Opens on Monday at 09:30");
     }
 }
